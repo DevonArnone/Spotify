0ad1172a1f1fb13c5e855d62a05be70bab711f68
Devon Arnone <87498263+DevonArnone@users.noreply.github.com>
2025-09-17 21:46:56 -0400

    Initial Commit

---
+++ spotify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spotify
+//
+//  Created by Devon Arnone on 9/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spotify/spotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotifyApp.swift
+//  spotify
+//
+//  Created by Devon Arnone on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct spotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
